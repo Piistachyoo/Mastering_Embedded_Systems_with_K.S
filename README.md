c06e9af2aa6943ee4f58b095e43f0f36cc86c6f1
Omar Yamany <omarmyamany@gmail.com>
2023-11-10 09:02:53 +0200

    Upload U15 Lab3

--- README.md
+++ README.md
@@ -19,9 +19,9 @@
 - ##### Unit 11: Second Term Project ![](https://geps.dev/progress/100)
 -----
 -----
-### Third Term: ![](https://geps.dev/progress/81)
+### Third Term: ![](https://geps.dev/progress/85)
 - ##### Unit 12: Advanced Topics ![](https://geps.dev/progress/67)
 - ##### Unit 13: Mastering CAN Protocol ![](https://geps.dev/progress/100)
 - ##### Unit 14: Mastering ARM Cortex ![](https://geps.dev/progress/100)
-- ##### Unit 15: Create Your Own RTOS ![](https://geps.dev/progress/57)
+- ##### Unit 15: Create Your Own RTOS ![](https://geps.dev/progress/71)
 -----
---
+++ Unit15_Create Your Own RTOS/Session 3/Src/main.c
@@ -0,0 +1,80 @@
+/*************************************************************************/
+/* Author        : Omar Yamany                                    		 */
+/* Project       : OmarOS  	                             				 */
+/* File          : main.c 			                     				 */
+/* Date          : Nov 7, 2023                                           */
+/* Version       : V1                                                    */
+/* GitHub        : https://github.com/Piistachyoo             		     */
+/*************************************************************************/
+
+#include "scheduler.h"
+
+Task_ref Task1, Task2, Task3;
+uint8	 Task1LED, Task2LED, Task3LED, Task4LED;
+
+void Task_1(void);
+
+void Task_2(void);
+
+void Task_3(void);
+
+int main(void)
+{
+	OmarOS_errorTypes retval = noError;
+	/* HW_Init (Initialize Clock tree, Reset Controller) */
+	HW_Init();
+	retval |= OmarOS_Init();
+
+	Task1.Stack_Size = 1024;
+	Task1.pf_TaskEntry = Task_1;
+	Task1.Priority = 3;
+	Task1.AutoStart = FALSE;
+	STRING_copy((uint8*)"Task_1", (uint8*)&Task1.TaskName);
+
+	Task2.Stack_Size = 1024;
+	Task2.pf_TaskEntry = Task_2;
+	Task2.Priority = 3;
+	Task2.AutoStart = FALSE;
+	STRING_copy((uint8*)"Task_2", (uint8*)&Task2.TaskName);
+
+	Task3.Stack_Size = 1024;
+	Task3.pf_TaskEntry = Task_3;
+	Task3.Priority = 4;
+	Task3.AutoStart = FALSE;
+	STRING_copy((uint8*)"Task_3", (uint8*)&Task3.TaskName);
+
+	retval |= OmarOS_CreateTask(&Task1);
+	retval |= OmarOS_CreateTask(&Task2);
+	retval |= OmarOS_CreateTask(&Task3);
+
+	OmarOS_ActivateTask(&Task1);
+	OmarOS_ActivateTask(&Task2);
+	OmarOS_ActivateTask(&Task3);
+
+	OmarOS_StartOS();
+
+    while(1){
+
+    }
+}
+
+void Task_1(void){
+	while(1){
+		/* Taskl Code */
+		Task1LED ^= 1;
+	}
+}
+
+void Task_2(void){
+	while(1){
+		/* Task2 Code */
+		Task2LED ^= 1;
+	}
+}
+
+void Task_3(void){
+	while(1){
+		/* Task3 Code */
+		Task3LED ^= 1;
+	}
+}
